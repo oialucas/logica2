fe21e9ad340cdd655a183698b17d54aedbbd3c56
oialucas <gui_feitosa9@hotmail.com>
2016-04-22 10:32:28 -0300

    Update Main.java

--- src/br/unifor/cct/cc/logica/pratica/Main.java
+++ src/br/unifor/cct/cc/logica/pratica/Main.java
@@ -8,7 +8,12 @@ package br.unifor.cct.cc.logica.pratica;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int [] matriculas = new int [1020, 1042, 1112, 1132, 1152, 1201, 1213, 1232, 1256, 1271];
+		String [] nomes = new String [Ailton, Marlos, Rebeca, Marco, Jane, Pedro, Raquel, Paulo, Vitor, Mara];
+		doubles [][] notas = {{7.5 8.9 6.5}, {8.2 7.3 9.4} , {10.0 9.6 7.1} , 
+							 {6.8 7.5 8.1} , {8.8 7.4 9.2} , {8.3 7.9 9.0} ,
+							 {7.2 8.7 9.8} , {8.3 7.7 8.9} , {7.8 7.7 8.5}, {8.0 7.0 9.0}};
+		
 
 	}
 
