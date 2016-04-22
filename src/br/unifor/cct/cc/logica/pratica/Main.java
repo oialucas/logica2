cfce5d1bc7399d5fd603d1c2574431edd7ce791b
eguimaraes15 <eguimaraes@unifor.br>
2016-04-22 09:48:23 -0300

    Subindo o projeto.
    Fazendo o commit.

---
+++ src/br/unifor/cct/cc/logica/pratica/Main.java
@@ -0,0 +1,15 @@
+package br.unifor.cct.cc.logica.pratica;
+
+/**
+ * 
+ * @author Everton Guimaraes
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
